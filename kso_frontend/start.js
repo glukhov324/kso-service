ca4122ad5ba7d74fbe3f0bae24e68733252e035e
glukhov324 <glukhovv.nn@gmail.com>
2025-06-27 07:08:59 +0700

    update front port settings

--- kso_frontend/start.js
+++ kso_frontend/start.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const { exec } = require('child_process');
 
-const FRONT_PORT = process.env.FRONT_PORT || 3000;
+const FRONT_PORT = process.env.PORT || 3000;
 
 exec(`cross-env PORT=${FRONT_PORT} react-scripts start`, {
   stdio: 'inherit'
