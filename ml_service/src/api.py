0c5f2dd526d683f0398b8521823304478f73158d
glukhov324 <glukhovv.nn@gmail.com>
2025-07-24 07:13:10 +0700

    feat(Api): update api prefix to /api

--- ml_service/src/api.py
+++ ml_service/src/api.py
@@ -11,13 +11,13 @@ from src.schemas import PipelinePrediction, Msg
 
 
 
-router = APIRouter(prefix="/pair_images")
+router = APIRouter(prefix="/api")
 
 
-@router.post('/predict', response_model=PipelinePrediction)
+@router.post("/predict", response_model=PipelinePrediction)
 async def a_b_label_predict(file_a: UploadFile, 
                             file_b: UploadFile, 
-                            user_label: str = Form(...)) -> PipelinePrediction:
+                            user_label: str = Form(...)):
 
     if user_label not in cls_models_dict.keys():
         return JSONResponse(content=Msg(msg="Для указанного названия товара нет классификатора").model_dump(),
@@ -26,6 +26,10 @@ async def a_b_label_predict(file_a: UploadFile,
     data_a = await file_a.read()
     data_b = await file_b.read()
 
-    pipeline_response = pipeline_prediction(data_a, data_b, user_label)
+    pipeline_response = pipeline_prediction(
+        data_a, 
+        data_b, 
+        user_label
+    )
 
     return pipeline_response
