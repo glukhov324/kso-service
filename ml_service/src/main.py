e389505e1c79a744631ee691fef6fc33669dccc7
glukhov324 <glukhovv.nn@gmail.com>
2025-07-24 07:29:54 +0700

    feat(Api): change tags, title, description, version

--- ml_service/src/main.py
+++ ml_service/src/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.api import router
+from src.settings import settings
 
 
 
-app = FastAPI()
+app = FastAPI(title=settings.APP_TITLE,
+              description=settings.APP_DESCRIPTION,
+              version=settings.APP_VERSION)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -12,4 +16,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.include_router(router)
+
+app.include_router(router,
+                   tags=["Predict"])
